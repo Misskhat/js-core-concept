4fa60aeae01ee3c28b88234a73066bff8424249c
Misskhat <misskhat.hossen@gmail.com>
2025-09-02 17:07:44 +0600

    implicity conversion file updated

---
+++ implicity-conversion.js
@@ -0,0 +1,29 @@
+const a = 5;
+const b = '5';
+
+// if we add both numbers
+const sum = a + b;
+console.log(sum); // 55
+
+// if we deduct bot numbers
+const deduct = a - b;
+console.log(deduct); // 0
+
+// if multiple both numbers
+const multiple = a * b;
+console.log(multiple); // 25
+
+// if divided both numbers
+const divided = a / b;
+console.log(divided); // 1
+
+// if use modulo both number
+
+const modulo = a / b;
+console.log(modulo); // 1
+
+
+
+/**
+ * So, if we add string and number in js then it's concat both numberS but other math operators are working as like mathematical.
+ */
